15697ac247a71f8dfdf8ed053bca8ba953a09933
bugrahandelioglu <bugrahandelioglu@outlook.com>
2021-08-24 14:28:27 +0300

    bugra-uptade-v0.8

--- main.go
+++ main.go
@@ -17,11 +17,14 @@ type album struct {
 	Price  float64 `json:"price"`
 }
 
-var albums = []album{
+/*var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
+*/
+
+var albums = "merhaba"
 
 func getAlbums(c *gin.Context) {
 	fmt.Println("Ulooo", c.Request)
