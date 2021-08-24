f900b2fcc819f1eb852109006e468bf5049a3fee
bugrahandelioglu <bugrahandelioglu@outlook.com>
2021-08-24 13:56:59 +0300

    bugra-testv-0.2

--- main.go
+++ main.go
@@ -11,25 +11,29 @@ import (
 )
 
 // album represents data about a record album.
-type album struct {
+/*type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
 }
-
+*/
 type simpleJson struct {
 	//testa string `json:"testa"`
 }
 
 // albums slice to seed record album data.
-var albums = []album{
+/*var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
+*/
+// getAlbums responds with the list of all albums as JSON.
 
-//getAlbums responds with the list of all albums as JSON.
+func albums(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("GİRİŞ EKRANI"))
+}
 func getAlbums(c *gin.Context) {
 	//fmt.Printf("Ulooo", c.Request)
 	c.IndentedJSON(http.StatusOK, albums)
@@ -37,18 +41,18 @@ func getAlbums(c *gin.Context) {
 
 // postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
-	var newAlbum album
+	//var newAlbum album
 
 	// Call BindJSON to bind the received JSON to
 	// newAlbum.
-	if err := c.BindJSON(&newAlbum); err != nil {
-		return
-	}
-
-	// Add the new album to the slice.
-	albums = append(albums, newAlbum)
-	c.IndentedJSON(http.StatusCreated, newAlbum)
+	//if err := c.BindJSON(&newAlbum); err != nil {
+	return
 }
+
+// Add the new album to the slice.
+//	albums = append(albums, newAlbum)
+//c.IndentedJSON(http.StatusCreated, newAlbum)
+//}
 
 func testaPost(c *gin.Context) {
 	var newTesta simpleJson
@@ -78,7 +82,8 @@ func main() {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
 
-	router.POST("/albums", getAlbums)
+	http.HandleFunc("/albums", albums)
+	router.GET("/albums", getAlbums)
 	router.POST("/new", testaPost)
 
 	router.Run(":" + port)
