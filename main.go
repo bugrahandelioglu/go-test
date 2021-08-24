05245965b40f1ebcc82c4d5cb8d2b4ff14d014ef
bugrahandelioglu <bugrahandelioglu@outlook.com>
2021-08-24 15:31:11 +0300

    bugra-uptade-v0,11

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 	_ "github.com/heroku/x/hmetrics/onload"
 )
 
-type album struct {
+/*type album struct {
 	ID     string  `json:"id"`
 	Title  string  `json:"title"`
 	Artist string  `json:"artist"`
 	Price  float64 `json:"price"`
-}
+}*/
 
 /*var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -42,10 +42,10 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
-	router.GET("/", func(c *gin.Context) {
+	router.POST("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
-	router.GET("/albums", getAlbums)
+	router.POST("/albums", getAlbums)
 
 	router.Run(":" + port)
 }
