190a732f2c9e8beebe22e3e1a1766f08b5fff66e
bugrahandelioglu <bugrahandelioglu@outlook.com>
2021-08-25 16:55:53 +0300

    bugra-uptade-v0,16

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ type album struct {
 
 // albums slice to seed record album data.
 var albums = []album{
-	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	/*{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},*/
 }
 
 func main() {
@@ -45,7 +45,6 @@ func main() {
 	router.GET("/albums/:id", getAlbumByID)
 	router.GET("/hubbox", hubbox)
 	router.POST("/albums", postAlbums)
-
 	router.Run(":" + port)
 }
 
